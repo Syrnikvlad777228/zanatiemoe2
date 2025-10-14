0ef9c0bad83d6ad66fbcd594217ffc01012c61f9
Vlad <dale2010.04.07@gmail.com>
2025-10-14 16:30:22 +0300

    сделал 2ое задание

--- nomer2/bin/nomer2.dart
+++ nomer2/bin/nomer2.dart
@@ -1,5 +1,13 @@
+import 'dart:io';
 import 'package:nomer2/nomer2.dart' as nomer2;
 
 void main(List<String> arguments) {
-  print('Hello world: ${nomer2.calculate()}!');
+  print("Введите х первой клетки");
+  final int a = int.parse(stdin.readLineSync() ?? "0");
+  print("Введите у первой клетки");
+  final int b = int.parse(stdin.readLineSync() ?? "0");
+  print("Введите х второй клетки");
+  final int a1 = int.parse(stdin.readLineSync() ?? "0");
+  print("Введите у второй клетки");
+  final int b1 = int.parse(stdin.readLineSync() ?? "0");
 }
