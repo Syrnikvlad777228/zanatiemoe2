a63b25f5a1653f93c8829b4ae1fa062468f14cb8
Vlladik <dale2010.04.07@gmail.com>
2025-10-27 22:07:54 +0300

    ДЗ 1ОЕ ЕСТЬ ЖЕ

---
+++ dz_cikl_z1/bin/dz_cikl_z1.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:dz_cikl_z1/dz_cikl_z1.dart' as dz_cikl_z1;
+
+void main(List<String> arguments) {
+ int troiniki = int.parse(stdin.readLineSync()!);
+ int sum = 0;
+for (var i = 0; i < troiniki; i++) {
+  int dirki = int.parse(stdin.readLineSync()!);
+  sum+= dirki;
+}
+print("Всего чайников = ${sum - troiniki + 1}");
+
+
+}
