a95a09661df0eb9b3267f1c7d02cde4cb8e6ac6e
Vlad <dale2010.04.07@gmail.com>
2025-10-28 16:30:31 +0300

    z6

---
+++ Listi/urok_listi_z6/bin/urok_listi_z6.dart
@@ -0,0 +1,8 @@
+import 'package:urok_listi_z6/urok_listi_z6.dart' as urok_listi_z6;
+
+void main(List<String> arguments) {
+  List<String> names = ['Anna','Igor','Dasha','Ivan','Olga'];
+  names.sort();
+  print(names[0]);
+  print(names[names.length - 1]);
+}
