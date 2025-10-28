6f4cfc3767adc2d90ae7b3ca79e3832715b476a3
Vlad <dale2010.04.07@gmail.com>
2025-10-28 15:36:17 +0300

    сделал 1ое задание лист

---
+++ Listi/urok_list_z1/bin/urok_list_z1.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+
+import 'package:urok_list_z1/urok_list_z1.dart' as urok_list_z1;
+
+void main(List<String> arguments) {
+  List m = [1,2,3,4,5];
+  List str = ["Vasa","Peta","Igor"];
+  List fl = [true,false,true];
+  print(m);
+  print(str);
+  print(fl);
+}
---
+++ Listi/urok_list_z1/test/urok_list_z1_test.dart
@@ -0,0 +1,8 @@
+import 'package:urok_list_z1/urok_list_z1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
